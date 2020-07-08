a46f9f89dad0b399b19291385ebf2fc10f68ece3
Zewsy <47224879+Zewsy@users.noreply.github.com>
2020-07-08 14:50:44 +0200

    Simplify routing

--- frontend/web/src/app/app-routing.module.ts
+++ frontend/web/src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MainLayoutComponent } from './core/main-layout/main-layout.component';
+import { MainPageComponent } from './core/main-page/main-page.component';
 import { LoginComponent } from './core/login/login.component';
 import { RegistrationComponent } from './core/registration/registration.component';
 
 const routes: Routes = [
-  { path: '',
-    component: MainLayoutComponent,
-    children: [
-      { path: 'attack', loadChildren: () => import('./features/attack/attack.module').then(m => m.AttackModule) },
-      { path: 'buildings', loadChildren: () => import('./features/buildings/buildings.module').then(m => m.BuildingsModule) }
-    ]
-  },
+  { path: '', component: MainPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent }
 ];
--- frontend/web/src/app/app.module.ts
+++ frontend/web/src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './core/login/login.component';
 import { RegistrationComponent } from './core/registration/registration.component';
-import { MainLayoutComponent } from './core/main-layout/main-layout.component';
+import { MainPageComponent } from './core/main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistrationComponent,
-    MainLayoutComponent
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SharedModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
