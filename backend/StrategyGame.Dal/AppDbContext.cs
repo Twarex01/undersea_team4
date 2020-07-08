bf1251ebc2a9b589cf2fb48a83ea33672c2618cf
Fehérvári Attila <fehervari.attila@encosoft.hu>
2020-07-08 11:17:58 +0200

    Init migráció létrehozva

--- backend/StrategyGame.Dal/AppDbContext.cs
+++ backend/StrategyGame.Dal/AppDbContext.cs
@@ -2,11 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using StrategyGame.Model;
 
 namespace StrategyGame.Dal
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Entity> MyProperty { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
