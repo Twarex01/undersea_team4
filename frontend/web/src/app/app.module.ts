242a9f41d50e737f341be7487aeb7de0feeabe0d
Zewsy <47224879+Zewsy@users.noreply.github.com>
2020-07-09 12:35:15 +0200

    Status Bar half-done

--- frontend/web/src/app/app.module.ts
+++ frontend/web/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './core/login/login.component';
 import { RegistrationComponent } from './core/registration/registration.component';
 import { MainLayoutComponent } from './core/main-layout/main-layout.component';
+import { StatusBarComponent } from './core/status-bar/status-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistrationComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    StatusBarComponent
   ],
   imports: [
     BrowserModule,
