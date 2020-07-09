46b0fb8f5e3eb8abf1e7aad83a8841cf0d69d96e
csikosaron98 <csikosaron1998@gmail.com>
2020-07-09 16:21:06 +0200

    Battle mergeconflict

---
+++ backend/StrategyGame.Api/Controllers/Battle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StrategyGame.Bll.DTO;
+
+namespace StrategyGame.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BattleController : ControllerBase
+    {
+        // POST api/attack
+        [HttpPost]
+        [Authorize]
+        public IActionResult Attack([FromBody] BattleDTO battleDTO)
+        {
+            throw new NotImplementedException("TODO");
+        }
+    }
+}
