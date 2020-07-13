0a63f55fbed0e16bf02eb91afd44dbcd58ab22ba
deviladam <szekely.adam.llg@gmail.com>
2020-07-13 12:23:52 +0200

    PurchaseCountryUpgradeAsync init

--- backend/StrategyGame.Bll/Services/PurchaseService.cs
+++ backend/StrategyGame.Bll/Services/PurchaseService.cs
@@ -3,6 +3,7 @@ using StrategyGame.Bll.DTO;
 using StrategyGame.Dal;
 using StrategyGame.Model;
 using StrategyGame.Model.BuildingTypes;
+using StrategyGame.Model.UpgradeTypes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,30 @@ namespace StrategyGame.Bll.Services
 {
 	public class PurchaseService : IPurchaseService
 	{
-		private readonly AppDbContext _appDbContext = null;
+        private enum BuildingIds
+        {
+			FLOW_REGULATOR_ID = 1,
+			REEF_FORT_ID
+        }
+
+		private enum UpgradesIds
+        {
+			ALCHEMY_ID = 1,
+			CORALL_WALL_ID,
+			MARTIAL_ARTS_ID,
+			MUD_HARVESTER_ID,
+			MUD_TRACTOR_ID,
+			SONAR_CANNON_ID
+		}
+
+        private enum UnitsIds
+        {
+			ASSAULT_SEAL_ID = 1,
+			BATTLE_SEAHORSE_ID,
+			LASER_SHARK_ID
+        }
+
+        private readonly AppDbContext _appDbContext = null;
 
 		public PurchaseService(AppDbContext appDbContext)
 		{
@@ -24,17 +48,18 @@ namespace StrategyGame.Bll.Services
 		{
 			var country = await _appDbContext.Countries.Where(c => c.ID == countryId).SingleOrDefaultAsync();
 			var buildingInprogress = country.Buildings.Where(b => b.CoutryID == countryId).Any(u => u.Progress > 0);
-			// There is a building inprogress
+			// 1 - There is a building inprogress
 			if (buildingInprogress) return await new Task<int>(() => 1);
 
 			var building = await _appDbContext.Buildings.Where(b => countryId == b.CoutryID && buildingId == b.BuildingDataID).SingleOrDefaultAsync();
 			var buildingData = await _appDbContext.BuildingData.Where(data => data.ID == buildingId).SingleOrDefaultAsync();
+			// 2 - Building not found
+			if (buildingData == null) return await new Task<int>(() => 2);
+
 			if (building == null)
 			{
-				// Building type not found
-				if (buildingData == null ) return await new Task<int>(() => 2);
-				switch (buildingId) {
-					case 1: // Áramlásirányító
+				switch ((BuildingIds)buildingId) {
+					case BuildingIds.FLOW_REGULATOR_ID: // Áramlásirányító
 						building = new FlowRegulator
 						{
 							Count = 0,
@@ -42,7 +67,7 @@ namespace StrategyGame.Bll.Services
 							CoutryID = countryId
 						};
 						break;
-					case 2: // Zátonyvár
+					case BuildingIds.REEF_FORT_ID: // Zátonyvár
 						building = new ReefFort
 						{
 							Count = 0,
@@ -58,23 +83,95 @@ namespace StrategyGame.Bll.Services
 				
 			}
 
-			//not enough resources
+			// 3 - not enough resources
 			if (country.Resources.Find(r => r.ID == buildingData.PriceUnitID).Amount < buildingData.Price) return await new Task<int>(() => 3);
 			country.Resources.Find(r => r.ID == buildingData.PriceUnitID).Amount -= buildingData.Price;
 			building.Progress = 5; //todo: check it
 			
 			await _appDbContext.SaveChangesAsync();
-			throw new NotImplementedException("TODO");
+
+			// 0 - ok
+			return await new Task<int>(() => 0);
 		}
 
 		public async Task<int> PurchaseCountryUnitsAsync(int countryId, List<UnitDTO> army)
 		{
+            foreach (var unitDto in army)
+            {
+				unitDto.
+            }
+
 			throw new NotImplementedException("TODO");
 		}
 
-		public async Task<int> PurchaseCountryUpgradeAsync(int countryId, int BuildingId)
+		public async Task<int> PurchaseCountryUpgradeAsync(int countryId, int upgradeId)
 		{
-			throw new NotImplementedException("TODO");
+			var country = await _appDbContext.Countries.Where(c => c.ID == countryId).SingleOrDefaultAsync();
+			var upgradeInprogress = country.Upgrades.Where(b => b.CoutryID == countryId).Any(u => u.Progress > 0);
+			// 1 - There is a upgrade inprogress
+			if (upgradeInprogress) return await new Task<int>(() => 1);
+
+			var upgrade = await _appDbContext.Upgrades.Where(b => countryId == b.CoutryID && upgradeId == b.UpgradeDataID).SingleOrDefaultAsync();
+			var upgradeData = await _appDbContext.UpgradeData.Where(data => data.ID == upgradeId).SingleOrDefaultAsync();
+			// 2 - Upgrade not found
+			if (upgradeData == null) return await new Task<int>(() => 2);
+
+			if (upgrade == null) {
+                switch ((UpgradesIds)upgradeId)
+                {
+					case UpgradesIds.ALCHEMY_ID:
+						upgrade = new Alchemy
+						{
+							CoutryID = countryId,
+							UpgradeDataID = upgradeId
+						};
+						break;
+					case UpgradesIds.CORALL_WALL_ID:
+						upgrade = new CoralWall
+						{
+							CoutryID = countryId,
+							UpgradeDataID = upgradeId
+						};
+						break;
+					case UpgradesIds.MARTIAL_ARTS_ID:
+						upgrade = new MartialArts
+						{
+							CoutryID = countryId,
+							UpgradeDataID = upgradeId
+						};
+						break;
+					case UpgradesIds.MUD_HARVESTER_ID:
+						upgrade = new MudHarvester
+						{
+							CoutryID = countryId,
+							UpgradeDataID = upgradeId
+						};
+						break;
+					case UpgradesIds.MUD_TRACTOR_ID:
+						upgrade = new MudTractor
+						{
+							CoutryID = countryId,
+							UpgradeDataID = upgradeId
+						};
+						break;
+					case UpgradesIds.SONAR_CANNON_ID:
+						upgrade = new SonarCannon
+						{
+							CoutryID = countryId,
+							UpgradeDataID = upgradeId
+						};
+						break;
+					default:
+                        break;
+                }
+
+				await _appDbContext.Upgrades.AddAsync(upgrade);
+            }
+
+			upgrade.Progress = 15;
+
+			// 0 - ok
+			return await new Task<int>(() => 0);
 		}
 	}
 }
