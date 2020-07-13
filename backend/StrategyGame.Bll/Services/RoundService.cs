55268646ca556b7e711ac895113525d609db8a2e
Fehérvári Attila <fehervari.attila@encosoft.hu>
2020-07-13 12:11:02 +0200

    BuildTime és ScoreValue

--- backend/StrategyGame.Bll/Services/RoundService.cs
+++ backend/StrategyGame.Bll/Services/RoundService.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using StrategyGame.Dal;
 using StrategyGame.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +16,7 @@ namespace StrategyGame.Bll.Services
 
         private AppDbContext _dbContext;
         private UserManager<User> _userManager;
+        private Random soldierMoraleGenerator = new Random();
 
         public RoundService(AppDbContext dbContext, UserManager<User> userManager)
         {
@@ -26,15 +29,68 @@ namespace StrategyGame.Bll.Services
             var pearls = country.Resources.SingleOrDefault(r => r.ResourceDataID == ResourceData.Pearl.ID);
             pearls.Amount += (int)Math.Truncate( pearls.ProductionBase * pearls.ProductionMultiplier);
         }
-        public async void SimulateRound() //nem jó még
-        {
-            var PlayerList = _userManager.Users.ToList();
 
-            foreach(var player in PlayerList)
+        private void GenerateCoralIncome(Country country)
+        {
+            var corals = country.Resources.SingleOrDefault(r => r.ResourceDataID == ResourceData.Coral.ID);
+            corals.Amount += (int)Math.Truncate(corals.ProductionBase * corals.ProductionMultiplier);
+        }
+
+        private void PaySoldiers(Country country)
+        {
+            
+            foreach(var unit in country.Units)
             {
-                GeneratePearlIncome(player.Country);
+
+                country.Resources.Where(c => c.ResourceDataID == unit.UnitData.SalaryUnitID).FirstOrDefault().Amount -= unit.UnitData.Salary;  //TESZTELNI!! a FoD miatt null pointer exception veszély
+            }
+            
+        }
+
+        private void ProceedWithUpgrade(Country country)
+        {
+            var currentlyUpgrading = country.Upgrades.Where(u => u.Progress > 0).SingleOrDefault(); //elvileg nem lehet 1-nél több eredmény
+            currentlyUpgrading.Progress--;
+            if (currentlyUpgrading.Progress == 0) currentlyUpgrading.ApplyEffects();
+            
+          
+        }
+        private void ProceedWithBuilding(Country country)
+        {
+            var currentlyBuilding = country.Buildings.Where(u => u.Progress > 0).SingleOrDefault(); //elvileg nem lehet 1-nél több eredmény
+            currentlyBuilding.Progress--;
+            if (currentlyBuilding.Progress == 0) currentlyBuilding.ApplyEffect();
+        }
+        public async Task SimulateRound() 
+        {
+            
+            var countryList = await _dbContext.Countries
+                .Include(c => c.Buildings).ThenInclude(b => b.BuildingData)
+                .Include(c => c.Upgrades).ThenInclude(u => u.UpgradeData)
+                .Include(c => c.Units).ThenInclude(u => u.UnitData)
+                .Include(c => c.Resources).ThenInclude(r => r.ResourceData)
+                .ToListAsync();
+
+            var battles = await _dbContext.Battles
+                .Include(b=> b.AttackingCountry)
+                .Include(b => b.DefendingCountry)
+                .Include(b => b.AttackingUnits).ThenInclude(u=> u.UnitData)
+                .ToListAsync();
+
+
+            // változások az egyes országokban
+            foreach( var country in countryList)
+            {
+                GeneratePearlIncome(country);
+                GenerateCoralIncome(country);
+                PaySoldiers(country);
+                ProceedWithUpgrade(country);
+                ProceedWithBuilding(country);
+
             }
 
+            //Harc
+            
 
 
         }
--- backend/StrategyGame.Model/UpgradeData.cs
+++ backend/StrategyGame.Model/UpgradeData.cs
@@ -6,14 +6,16 @@ namespace StrategyGame.Model
 {
     public class UpgradeData
     {
-        public static UpgradeData Alchemy = new UpgradeData { ID = 1, Name = "Alkímia" };
-        public static UpgradeData CoralWall = new UpgradeData { ID = 2, Name = "Korall fal" };
-        public static UpgradeData MartialArts = new UpgradeData { ID = 3, Name = "Vízalatti harcművészetek" };
-        public static UpgradeData MudHarvester = new UpgradeData { ID = 4, Name = "Iszap Kombájn" };
-        public static UpgradeData MudTractor = new UpgradeData { ID = 5, Name = "Iszap Traktor" };
-        public static UpgradeData SonarCannon = new UpgradeData { ID = 6, Name = "Szonár ágyú" };
+        public static UpgradeData Alchemy = new UpgradeData { ID = 1, Name = "Alkímia", UpgradeTime=15 };
+        public static UpgradeData CoralWall = new UpgradeData { ID = 2, Name = "Korall fal", UpgradeTime = 15 };
+        public static UpgradeData MartialArts = new UpgradeData { ID = 3, Name = "Vízalatti harcművészetek", UpgradeTime = 15 };
+        public static UpgradeData MudHarvester = new UpgradeData { ID = 4, Name = "Iszap Kombájn", UpgradeTime = 15 };
+        public static UpgradeData MudTractor = new UpgradeData { ID = 5, Name = "Iszap Traktor", UpgradeTime = 15 };
+        public static UpgradeData SonarCannon = new UpgradeData { ID = 6, Name = "Szonár ágyú", UpgradeTime = 15 };
+     
         public int ID { get; set; }
         public string Name { get; set; }
-        
+        public int UpgradeTime { get; set; }
+
     }
 }
