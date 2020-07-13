2ad87076d7c35ff0cb8db903c2b1342efe203ce5
Twarex01 <Twarex01@gmail.com>
2020-07-13 08:30:00 +0200

    BattleService néhány fgv.

---
+++ backend/StrategyGame.Bll/Services/BattleService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using StrategyGame.Dal;
+using StrategyGame.Model;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StrategyGame.Bll.Services
+{
+    public class BattleService : IBattleService
+    {
+
+        private AppDbContext _context;
+
+        public BattleService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<int> CountUnitsOfTypeNotAtHomeAsync(int countryId, int unitDataId)
+        {
+
+            var count = await _context.AttackingUnits.Where(a => a.UnitDataID == unitDataId && a.Battle.AttackingCountryID == countryId)
+                .SumAsync(k => k.Count).ConfigureAwait(false);
+
+            return count;
+        }
+
+        public async Task<int> CountUnitsOfTypeAtHomeAsync(int countryId, int unitDataId)
+        {
+
+            var count = await _context.Units.Where(u => u.UnitDataID == unitDataId && u.CoutryID == countryId)
+                .SumAsync(k => k.Count).ConfigureAwait(false);
+
+            count -= CountUnitsOfTypeNotAtHomeAsync(countryId, unitDataId).Result;
+
+            return count;
+        }
+
+        public async Task<int> CountAttackPowerInBattleAsync(int battleId)
+        {
+
+            var count = _context.AttackingUnits.Include(a => a.Battle).Include(a => a.UnitData).Where(a => a.BattleID == battleId).Sum(x => x.Count * x.UnitData.ATK);            
+
+            return count;
+        }
+
+
+
+
+    }
+}
---
+++ backend/StrategyGame.Bll/Services/IBattleService.cs
@@ -0,0 +1,11 @@
+﻿using StrategyGame.Model;
+
+namespace StrategyGame.Bll.Services
+{
+    public interface IBattleService
+    {
+        public int CountUnitsOfTypeAttacking(int countryId, int unitDataId);
+
+        public int CountUnitsOfTypeDefending(int countryId, int unitDataId);
+    }
+}
