77809350b64d61cb89afea80b54be950fff15a55
Zewsy <47224879+Zewsy@users.noreply.github.com>
2020-07-13 14:29:25 +0200

    Unit card item

---
+++ frontend/web/src/app/features/units/components/unit-card-item/unit-card-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unit-card-item',
+  templateUrl: './unit-card-item.component.html',
+  styleUrls: ['./unit-card-item.component.css']
+})
+export class UnitCardItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontend/web/src/app/features/units/units.module.ts
+++ frontend/web/src/app/features/units/units.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { UnitsRoutingModule } from './units-routing.module';
 import { UnitsPageComponent } from './pages/units.page/units.page.component';
 import { SharedModule } from '../../shared/shared.module';
+import { UnitCardItemComponent } from './components/unit-card-item/unit-card-item.component';
 
 
 @NgModule({
-  declarations: [UnitsPageComponent],
+  declarations: [UnitsPageComponent, UnitCardItemComponent],
   imports: [
     CommonModule,
     UnitsRoutingModule,
