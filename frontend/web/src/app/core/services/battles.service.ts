4776d2ef74f38c17b53c05612fdefac3c42bc931
Adam <szekely.adam.llg@gmail.com>
2020-07-15 01:07:52 +0200

    init battle.service and rankings.service

---
+++ frontend/web/src/app/core/services/battles.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BattlesService {
+
+  constructor( ) { }
+  /*Kellene hozzá egy új endpoint szerintem (illetve DTO). */
+}
