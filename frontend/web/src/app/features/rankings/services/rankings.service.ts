630edcfd27cf262bec59c73e2ab04c4d28ffcdb7
deviladam <szekely.adam.llg@gmail.com>
2020-07-15 10:38:28 +0200

    services replace

--- frontend/web/src/app/features/rankings/services/rankings.service.ts
+++ frontend/web/src/app/features/rankings/services/rankings.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { PlayersClient, PlayerDTO } from '../../shared/clients';
+import { PlayersClient, PlayerDTO } from '../../../shared/clients';
 
 @Injectable({
   providedIn: 'root'
