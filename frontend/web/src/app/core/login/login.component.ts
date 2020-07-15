c284d4d5e6cd88199ce505b044946b1b6423196f
Zewsy <47224879+Zewsy@users.noreply.github.com>
2020-07-15 09:38:16 +0200

    Login fix, logout

--- frontend/web/src/app/core/login/login.component.ts
+++ frontend/web/src/app/core/login/login.component.ts
@@ -7,33 +7,34 @@ import { AccountService } from '../services/account.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   error: boolean = false;
 
   loginForm = new FormGroup({
     userName: new FormControl(''),
-    password: new FormControl('')
+    password: new FormControl(''),
   });
 
-  constructor(private accountService: AccountService, private router: Router) { }
+  constructor(private accountService: AccountService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit() {
     this.error = false;
-    this.accountService.login(new LoginDTO(this.loginForm.value)).subscribe((data) => {
-      const reader = new FileReader();
-      reader.onload = () => localStorage.setItem("token", reader.result!.toString());
-      reader.readAsText(data!.data);
-      this.router.navigate(["/"]);
-    },
-    () => {
-      this.error = true;
-    });
+    this.accountService.login(new LoginDTO(this.loginForm.value)).subscribe(
+      (data) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          localStorage.setItem('token', reader.result!.toString());
+          this.router.navigate(["/"]);
+        };
+        reader.readAsText(data!.data);
+      },
+      () => {
+        this.error = true;
+      }
+    );
   }
-
 }
