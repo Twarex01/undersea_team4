515c3caa48db59186de9042cee2b4fad4a2ca3e3
deviladam <szekely.adam.llg@gmail.com>
2020-07-15 16:26:56 +0200

    ranking page bekötés

--- frontend/web/src/app/features/rankings/pages/rankings.page/rankings.page.component.ts
+++ frontend/web/src/app/features/rankings/pages/rankings.page/rankings.page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RankingsService } from '../../services/rankings.service';
 import { Observable } from 'rxjs';
 import { PlayerDTO } from '../../../../shared/clients';
+import { Player } from '../../palyer';
 
 @Component({
   selector: 'app-rankings.page',
@@ -10,16 +11,19 @@ import { PlayerDTO } from '../../../../shared/clients';
 })
 export class RankingsPageComponent implements OnInit {
 
-  
-  palyers$: Observable<PlayerDTO[]> | undefined;
+  players: Player[] = new Array<Player>(
+    {name: "Jani", id: 0, score: 222},
+    {name: "Jani2", id: 1, score: 122},
+    {name: "Jani233", id: 1, score: 12}
+    );
 
   constructor(private rankingsService: RankingsService) { }
 
   ngOnInit(): void {
   }
 
-  getPlayes(){
-    //this.rankingsService.getPlayerList().subscribe(palyers => this.palyers$ = palyers)
+  getPlayers() {
+    // this.rankingsService.getPlayerList().subscribe(list => this.players = list);
   }
 
 }
