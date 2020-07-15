03799fad43720027fdd15950bac05efce6e23bee
Fehérvári Attila <fehervari.attila@encosoft.hu>
2020-07-15 09:49:32 +0200

    controllerek összevonva (még üres)

--- backend/StrategyGame.Api/Controllers/DetailsController.cs
+++ backend/StrategyGame.Api/Controllers/DetailsController.cs
@@ -4,19 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using StrategyGame.Bll.DTO;
 
 namespace StrategyGame.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UnitsController : ControllerBase
+    public class DetailsController : ControllerBase
     {
-        //POST api/units
-        [HttpPost]
-        public List<UnitDetailsDTO> UnitsData([FromBody] int id)
-        {
-            throw new NotImplementedException("TODO");
-        }
     }
 }
