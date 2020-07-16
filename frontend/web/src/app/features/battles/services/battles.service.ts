faa7bca9070c4b0a19e1b7ee6c89bf48539ada5c
Zewsy <47224879+Zewsy@users.noreply.github.com>
2020-07-16 14:24:02 +0200

    Battles service

--- frontend/web/src/app/features/battles/services/battles.service.ts
+++ frontend/web/src/app/features/battles/services/battles.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
+import { BattleClient } from '../../../shared/clients';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Battle } from '../battle';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BattlesService {
 
-  constructor( ) { }
-  /*Kellene hozzá egy új endpoint szerintem (illetve DTO). */
+  constructor(private battleClient: BattleClient) { }
+
+  getCountryBattles(): Observable<Battle[]> {
+    return this.battleClient.getCountryBattles().pipe(
+      map((battleArrayDTO) => {
+        return battleArrayDTO.map((battleDTO) => ({
+          defenderName: battleDTO.defenderName!,
+          units: battleDTO.units!
+        }))
+      })
+    );
+  }
+
 }
