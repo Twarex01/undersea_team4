e7f16c157288eaa9a8bfaf366228b5dd04a0fa29
Zewsy <47224879+Zewsy@users.noreply.github.com>
2020-07-16 13:49:41 +0200

    Buildings service

--- frontend/web/src/app/features/buildings/building-details.ts
+++ frontend/web/src/app/features/buildings/building-details.ts
@@ -1,10 +1,8 @@
-export interface Building {
+export interface BuildingDetails {
     id: number;
-    imgSrc: string;
     name: string;
+    imageSrc: string;
     description: string;
-    count: number;
     price: number;
     priceType: string;
-    isSelected: boolean;
 }
