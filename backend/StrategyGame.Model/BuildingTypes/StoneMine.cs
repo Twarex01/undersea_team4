aef7f57e1c7cd183f593422b00b3975843f0032d
Twarex01 <Twarex01@gmail.com>
2020-07-20 16:55:39 +0200

    stoneMine alap

---
+++ backend/StrategyGame.Model/BuildingTypes/StoneMine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyGame.Model.BuildingTypes
+{
+    public class StoneMine : BuildingData
+    {
+        public override void ApplyEffect(Country country)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- backend/StrategyGame.Model/ResourceData.cs
+++ backend/StrategyGame.Model/ResourceData.cs
@@ -8,6 +8,12 @@
         public static ResourceData Pearl = new ResourceData { ID = 2, Name = "Gyöngy", 
             ImageURL = "https://public-v2links.adobecc.com/a6f48b49-2354-4be7-78c0-090bdb752a04/component?params=component_id%3A2767b629-5fa0-46a8-a9bb-d7a3f7bf0054&params=version%3A0&token=1595286086_fac29e2b_58e760f6d8b9aa02eda4f5c868eded6278ccce9b&api_key=CometServer1"
         };
+        public static ResourceData Stone = new ResourceData
+        {
+            ID = 3, Name = "Kő",
+            ImageURL = ""
+        };
+
         public static int TaxAmount = 25; // azért csak egy szám mert a játék működése szempontjából nem lenne értelmes a gyöngytől különböző erőforrást adózni
         public static int BasePopulation = 10;
         public static int BaseCoralProduction = 20;
