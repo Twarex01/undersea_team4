1ce683b730146e148ecb144410ee230dd8a37c03
Twarex01 <Twarex01@gmail.com>
2020-07-21 13:15:03 +0200

    Áthelyezés

--- backend/StrategyGame.Bll/Services/HttpResponseExceptionFilter.cs
+++ backend/StrategyGame.Bll/Services/HttpResponseExceptionFilter.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace StrategyGame.Api.Helper
+namespace StrategyGame.Bll
 {
     public class HttpResponseExceptionFilter : IActionFilter, IOrderedFilter
     {
