9e552c888cf7dc04f944f6b2d3db9d0bbf101788
csikosaron98 <csikosaron1998@gmail.com>
2020-07-21 16:57:34 +0200

    Spying függvény megkezdése - még javításra vár

--- backend/StrategyGame.Api/Controllers/BattleController.cs
+++ backend/StrategyGame.Api/Controllers/BattleController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using StrategyGame.Bll;
 using StrategyGame.Bll.DTO;
 using StrategyGame.Bll.Services;
 using System;
@@ -31,6 +30,7 @@ namespace StrategyGame.Api.Controllers
         [Authorize]
         [Route("Attack")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
 
         public async Task<IActionResult> AttackAsync([FromBody] BattleDTO battleDTO)
@@ -38,7 +38,14 @@ namespace StrategyGame.Api.Controllers
             var atkCountry = await _userService.GetCountryByUserID(User.Identity.Name);
             battleDTO.IdAtt = atkCountry.ID;
 
-             _battleService.SendAllTypesToAttack(battleDTO);
+            try
+            {
+                _battleService.SendAllTypesToAttack(battleDTO);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
 
             return Ok();
         }
