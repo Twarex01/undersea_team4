5bd289ad1fdd8e60dcba23d13a112183fa743b6b
Zewsy <47224879+Zewsy@users.noreply.github.com>
2020-07-22 09:32:38 +0200

    SharedModule import

--- frontend/web/src/app/features/spyinfo/spyinfo.module.ts
+++ frontend/web/src/app/features/spyinfo/spyinfo.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { SpyinfoRoutingModule } from './spyinfo-routing.module';
 import { SpyinfoPageComponent } from './pages/spyinfo.page/spyinfo.page.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
   declarations: [SpyinfoPageComponent],
   imports: [
     CommonModule,
-    SpyinfoRoutingModule
+    SpyinfoRoutingModule,
+    SharedModule
   ]
 })
 export class SpyinfoModule { }
--- frontend/web/src/app/features/spying/spying.module.ts
+++ frontend/web/src/app/features/spying/spying.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { SpyingRoutingModule } from './spying-routing.module';
 import { SpyingPageComponent } from './pages/spying.page/spying.page.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
   declarations: [SpyingPageComponent],
   imports: [
     CommonModule,
-    SpyingRoutingModule
+    SpyingRoutingModule,
+    SharedModule
   ]
 })
 export class SpyingModule { }
