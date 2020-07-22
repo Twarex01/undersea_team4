811eaebd599fa9abd88c59a4bbc866d2e2810bf7
Zewsy <47224879+Zewsy@users.noreply.github.com>
2020-07-22 10:25:53 +0200

    Spying page connections

--- frontend/web/src/app/features/spying/pages/spying.page/spying.page.component.ts
+++ frontend/web/src/app/features/spying/pages/spying.page/spying.page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AttackUnit } from '../../../attack/models/attack-unit';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { AttackPlayer } from '../../../attack/models/attack-player';
 
 @Component({
   selector: 'app-spying.page',
@@ -12,10 +13,12 @@ export class SpyingPageComponent implements OnInit {
   countryName: string = "";
   selectedPlayerId: number = -1;
   explorerUnits: AttackUnit;
+  players: AttackPlayer[] = [];
 
   constructor(private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    //TODO
   }
 
   onExplore() {
