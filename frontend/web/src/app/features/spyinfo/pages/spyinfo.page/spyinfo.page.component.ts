d3d912ddd86cac4b2e6c3ca2e47765a96e23be2b
Zewsy <47224879+Zewsy@users.noreply.github.com>
2020-07-22 11:04:51 +0200

    Spyinfo connections

--- frontend/web/src/app/features/spyinfo/pages/spyinfo.page/spyinfo.page.component.ts
+++ frontend/web/src/app/features/spyinfo/pages/spyinfo.page/spyinfo.page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Spyinfo } from '../../models/spyinfo';
+import { StatusNotificationService } from '../../../../core/services/status-notification.service';
 
 @Component({
   selector: 'app-spyinfo.page',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpyinfoPageComponent implements OnInit {
 
-  constructor() { }
+  spyinfos: Spyinfo[] = [];
+
+  constructor(private statusNotificationService: StatusNotificationService) { }
 
   ngOnInit(): void {
+
+  }
+
+  getSpyinfoData(): void {
+    
   }
 
 }
