ef546b7fc67fbd84ec3e9a91ab964f18e36725db
Zewsy <47224879+Zewsy@users.noreply.github.com>
2020-07-22 10:22:35 +0200

    Spying component logic

--- frontend/web/src/app/features/spying/components/spying/spying.component.ts
+++ frontend/web/src/app/features/spying/components/spying/spying.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AttackUnit } from '../../../attack/models/attack-unit';
+import { AttackPlayer } from '../../../attack/models/attack-player';
 
 @Component({
   selector: 'app-spying',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpyingComponent implements OnInit {
 
+  @Input() explorerUnits: AttackUnit;
+  @Input() players: AttackPlayer[];
+  @Output() selectedPlayerChanged = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedPlayerChanged(id: number) {
+    this.selectedPlayerChanged.emit(id);
   }
 
 }
