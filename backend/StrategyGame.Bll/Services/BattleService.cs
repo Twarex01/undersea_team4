26ed4bf06a8bd7ab47b4d7b34b973ab48cf34a67
Twarex01 <Twarex01@gmail.com>
2020-07-23 12:01:27 +0200

    battlereport cucc így first draftra elv kész

--- backend/StrategyGame.Bll/Services/BattleService.cs
+++ backend/StrategyGame.Bll/Services/BattleService.cs
@@ -289,8 +289,8 @@ namespace StrategyGame.Bll.Services
 					DefenderName = defCountry.Name,
 					Round = roundNumber,
 					AttackerArmy = reportedUnits,
-					Loot = null, //TODO
-					UnitsLost = null //TODO
+					Loot = null,
+					UnitsLost = null
 				};
 
 			if (ATKPower > DEFPower)
@@ -298,28 +298,42 @@ namespace StrategyGame.Bll.Services
 
 				battleHistory.Succesful = true;
 
+				var lostUnits = new List<LostUnit>();
+
 				foreach (int unitDataId in _context.UnitData.Select(u => u.ID))
 				{
 					int unitAtHomeLost = CountUnitsOfTypeAtHome(defCountry.ID, unitDataId);
 					if (unitAtHomeLost == 0) continue;
 					unitAtHomeLost = (int)Math.Ceiling(unitAtHomeLost * 0.1);
-					_context.Units.Where(u => u.CountryID == defCountry.ID && u.UnitDataID == unitDataId).SingleOrDefault().Count -= unitAtHomeLost;				
+					var unit = _context.Units.Include(u => u.UnitData).Where(u => u.CountryID == defCountry.ID && u.UnitDataID == unitDataId).SingleOrDefault();
+					unit.Count -= unitAtHomeLost;
+
+					lostUnits.Add(new LostUnit { LostAmount = unitAtHomeLost, UnitName = unit.UnitData.Name });
 				}
+
+				var loot = new List<Loot>();
 
 				foreach (int resourceDataId in _context.ResourceData.Select(u => u.ID))
 				{
+					var resource = _context.Resources.Include(r => r.ResourceData).Where(r => r.ResourceDataID == resourceDataId).FirstOrDefault();
 					int resourcesTaken = _context.Resources.Include(r => r.Country).Where(r => r.ResourceDataID == resourceDataId && r.CoutryID == defCountry.ID).Select(r => r.Amount).First();
+					//Ez így elég retek de nem akarom most piszkálni.
+
 					resourcesTaken = (int)Math.Ceiling(resourcesTaken * 0.5);
 					_context.Resources.Include(r => r.Country).Where(u => u.CoutryID == atkCountry.ID && u.ResourceDataID == resourceDataId).SingleOrDefault().Amount += resourcesTaken;
 					var defenderResource = _context.Resources.SingleOrDefault(r => r.ResourceDataID == resourceDataId && r.CoutryID == defCountry.ID);
 					if (defenderResource.Amount < resourcesTaken) defenderResource.Amount = 0;
-					else defenderResource.Amount -= resourcesTaken;					
+					else defenderResource.Amount -= resourcesTaken;
+
+					loot.Add(new Loot { ResourceName = resource.ResourceData.Name, Amount = resourcesTaken });
 				}
 			}
 			else
 			{
 
 				battleHistory.Succesful = false;
+
+				var lostUnits = new List<LostUnit>();
 
 				var allAttackingUnits = _context.AttackingUnits.Include(a => a.Battle).Include(a => a.UnitData).Where(a => a.BattleID == battleId).ToList();
 
@@ -328,7 +342,10 @@ namespace StrategyGame.Bll.Services
 					int unitAttackingLost = allAttackingUnits.Where(a => a.UnitData.ID == unitDataId).Select(a => a.Count).SingleOrDefault();
 					if (unitAttackingLost == 0) continue;
 					unitAttackingLost = (int)Math.Ceiling(unitAttackingLost * 0.1);
-					_context.Units.Where(u => u.CountryID == atkCountry.ID && u.UnitDataID == unitDataId).SingleOrDefault().Count -= unitAttackingLost;			
+					var unit = _context.Units.Include(u => u.UnitData).Where(u => u.CountryID == defCountry.ID && u.UnitDataID == unitDataId).SingleOrDefault();
+					unit.Count -= unitAttackingLost;
+
+					lostUnits.Add(new LostUnit { LostAmount = unitAttackingLost, UnitName = unit.UnitData.Name });
 				}
 			}
 
