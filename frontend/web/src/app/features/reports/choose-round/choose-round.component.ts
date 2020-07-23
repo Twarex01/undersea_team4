16f6ff13a3f7cb5680695ed75b076d35218b579e
Zewsy <47224879+Zewsy@users.noreply.github.com>
2020-07-23 13:45:29 +0200

    Choose round basic logic

--- frontend/web/src/app/features/reports/choose-round/choose-round.component.ts
+++ frontend/web/src/app/features/reports/choose-round/choose-round.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChooseRoundComponent implements OnInit {
 
+  chosenRound: number = 1;
+  actualRound: number = 20;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeRound(num: number) {
+    if(this.chosenRound == 1 && num < 0)  return;
+    if(this.chosenRound == this.actualRound && num > 0) return;
+    this.chosenRound += num;
   }
 
 }
