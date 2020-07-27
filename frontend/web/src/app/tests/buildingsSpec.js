29d5d7eef7ba164c917ff0a516b3f503d5b18f3b
Zewsy <47224879+Zewsy@users.noreply.github.com>
2020-07-27 15:19:31 +0200

    nextTestInit

--- frontend/web/src/app/tests/buildingsSpec.js
+++ frontend/web/src/app/tests/buildingsSpec.js
@@ -1,9 +1,23 @@
 const { element, browser } = require("protractor");
 
 describe("Buildings Page", () => {
-  it("should have buildings", () => {
+  function nextRound() {
+    var nextRoundBtn = element(by.id("nextRoundBtn"));
+    nextRound.click();
+  }
+
+  beforeEach(() => {
     browser.get("http://localhost:4200/buildings");
+  });
+
+  it("shouldn't be able to buy two buildings", () => {
     var buildings = element.all(by.css(".building-card-item"));
-    expect(buildings.count()).toBeGreaterThan(0);
+    var buyButton = element(by.id("buyBtn"));
+
+    buildings.first().click();
+    buyButton.click();
+
+    expect(buyButton.isEnabled()).toBe(false);
+
   });
 });
